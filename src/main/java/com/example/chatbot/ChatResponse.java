ee17a069d0d818e5a8d30898155b29c11b1fe2f2
Krishna Kumar V <krishnav1695@gmail.com>
2026-07-09 21:58:28 +0530

    Remove explicit constructors clashing with Lombok

--- src/main/java/com/example/chatbot/ChatResponse.java
+++ src/main/java/com/example/chatbot/ChatResponse.java
@@ -1,13 +1,6 @@
 package com.example.chatbot;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatResponse {
     private String response;
   
--- src/main/java/com/example/chatbot/dto/ChatRequest.java
+++ src/main/java/com/example/chatbot/dto/ChatRequest.java
@@ -1,13 +1,6 @@
 package com.example.chatbot.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ChatRequest {
     private String message;
     private String type; // Value will be "text" or "image"
